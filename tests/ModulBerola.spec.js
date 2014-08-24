440cd62f5dac97ad0140e05bd868d92d3b9c0183
Szmeku <szmeku@gmail.com>
2014-08-24 16:48:30 +0200

    improved power tests

--- tests/ModulBerola.spec.js
+++ tests/ModulBerola.spec.js
@@ -4,8 +4,25 @@ describe("Testy modułu berola:", function() {
         expect(ModulBerola.dodawanie(2, 4)).toBe(6);
     });
 
-    it("funkcja potegowanie powinna potegowac", function(){
-        expect(ModulBerola.potega(2, 3)).toBe(8);
-    });
-    //will insert additional tests here later
+    // testowanie potegowania
+    // [pierwszy argumont, drugi, wynik]
+    var argumentsAndResults = [
+        [20,2,400],
+        [3,2,9],
+        [2,3,8],
+        [4,0,1]
+    ];
+
+    for(var i in argumentsAndResults){
+        it("potega "+argumentsAndResults[i][0]
+            + '^'+argumentsAndResults[i][1] + ' ma być '
+            + argumentsAndResults[i][2] , function(){
+
+            expect(
+                ModulBerola.potega(argumentsAndResults[i][0],
+                                    argumentsAndResults[i][1])
+                ).toBe(argumentsAndResults[i][2]);
+
+        });
+    }
 });
